29cf12988eaf8d0b4ce8b70dede1fa6b1d13fb65
Hunter Webster <huntertwebster@gmail.com>
2021-06-30 17:42:13 -0500

    near completed week 1 assignment

---
+++ assignment/helloWorld.js
@@ -0,0 +1,11 @@
+console.log("Hello world!");
+for (let i=1; i < 2; i++) {
+    console.log("Hello Blaine!");
+}
+for (let i=2; i > 1; i--) {
+    console.log("Hello Mary!");
+}
+for (let i=1; i < 2; i++) {
+    console.log("Hello Emma!");
+}
+console.log("If you're reading this, you're an inspector and you probably know that this man pictured, is not me. Yes, I am an imposter already and its only week one.")
